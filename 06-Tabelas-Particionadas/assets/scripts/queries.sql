a420811a80c85c84d6d130ecaa4fb30f523f0514
infobarbosa <infobarbosa@yahoo.com.br>
2024-01-15 21:48:09 -0300

    Add partitioning queries for ecommerce.pedidos_part table

--- 06-Tabelas-Particionadas/assets/scripts/queries.sql
+++ 06-Tabelas-Particionadas/assets/scripts/queries.sql
@@ -1,4 +1,11 @@
---funcionou
+/*Considerando DATA_PEDIDO=STRING e DATA_CRIACAO=STRING*/
+SELECT *
+FROM "ecommerce"."pedidos_part" 
+WHERE date_parse(data_pedido,'%Y-%m-%d') = date_parse('2024-01-01','%Y-%m-%d') 
+AND cast(date_parse(data_criacao,'%Y-%m-%dT%H:%i:%s') as DATE ) = date_parse('2024-01-01','%Y-%m-%d') 
+limit 10;
+
+/*Considerando DATA_PEDIDO=DATE e DATA_CRIACAO=STRING*/
 SELECT *
 FROM "ecommerce"."pedidos_part" 
 where data_pedido = cast(date_parse('2024-01-01','%Y-%m-%d') as date)
@@ -39,3 +46,17 @@ FROM "ecommerce"."pedidos_part" p
 WHERE p.data_pedido = cast(date_parse('2024-01-01','%Y-%m-%d') as DATE)
 AND date_parse(p.data_criacao,'%Y-%m-%dT%H:%i:%s') = date_parse('2024-01-01T15:44:46','%Y-%m-%dT%H:%i:%s') 
 LIMIT 10;
+
+
+/*ADICIONANDO PARTIÇÕES*/
+SELECT * FROM "AwsDataCatalog"."ecommerce"."pedidos_part" limit 10;
+
+MSCK REPAIR TABLE ecommerce.pedidos_part;
+
+SHOW partitions ecommerce.pedidos_part;
+
+ALTER TABLE ecommerce.pedidos_part ADD PARTITION (data_pedido='2024-01-01') location 's3://lab-data-eng-202402-p4004/raw/ecommerce/pedidos/part/' ;
+
+ALTER TABLE ecommerce.pedidos_part ADD PARTITION (data_pedido='2024-01-02') location 's3://lab-data-eng-202402-p4004/raw/ecommerce/pedidos/part/' ;
+
+ALTER TABLE ecommerce.pedidos_part ADD PARTITION (data_pedido='2024-01-03') location 's3://lab-data-eng-202402-p4004/raw/ecommerce/pedidos/part/' ;
