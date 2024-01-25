9145053e7bf15c80a4c977718c14c3e41360ef89
infobarbosa <infobarbosa@yahoo.com.br>
2024-01-25 16:06:36 -0300

    Update Glue job configuration. retirados alguns parâmetros do json.

---
+++ 08-Glue-Job/assets/scripts/glue-job-ecommerce.sh
@@ -0,0 +1,20 @@
+# Variável de ambiente BUCKET_NAME
+export BUCKET_NAME=$(aws s3api list-buckets --query "Buckets[].Name" | grep 'lab-data-eng' | tr -d ' ' | tr -d '"' | tr -d ',')
+
+echo $BUCKET_NAME
+
+# Copiando o script para o S3
+aws s3 cp 08-Glue-Job/assets/scripts/glue-job-ecommerce.py s3://lab-data-eng-202402-p40041/scripts/glue-job-ecommerce.py
+
+# Criando as pastas no S3
+aws s3api put-object --bucket ${bucket_name} --key temp/
+aws s3api put-object --bucket ${bucket_name} --key spark-ui/
+
+# Criando o job
+aws glue create-job \
+    --name ecommerce_glue_job \
+    --role LabRole \
+    --command Name=glueetl,ScriptLocation=s3://${BUCKET_NAME}/scripts/glue-job-ecommerce.py \
+    --default-arguments '{"--TempDir": "s3://\'${BUCKET_NAME}\'/temp", "--spark-event-logs-path": "s3://\'${BUCKET_NAME}\'/spark-ui/"}' \
+    --cli-input-json "file://08-Glue-Job/assets/scripts/glue-job-ecommerce.json"
+
