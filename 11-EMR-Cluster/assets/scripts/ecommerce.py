2ff382eb0c5d8bc7c5594b68cbcab0bd910f0172
infobarbosa <infobarbosa@yahoo.com.br>
2024-01-25 18:46:00 -0300

    Update SparkSession configuration and write pedidos_parquet data to JSON

--- 11-EMR-Cluster/assets/scripts/ecommerce.py
+++ 11-EMR-Cluster/assets/scripts/ecommerce.py
@@ -4,13 +4,19 @@ from datetime import datetime
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
-spark = SparkSession\
-    .builder\
-    .appName("data-eng-lab")\
+spark = SparkSession \
+    .builder \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog") \
+    .config("spark.sql.catalog.spark_catalog.type", "hive") \
+    .appName("data-eng-lab") \
     .getOrCreate()
 
 spark.catalog.setCurrentDatabase("ecommerce")
-df = spark.sql("select * from clientes")
-df.show(5)
 
-df.write.format("json").mode("overwrite").save("s3://lab-data-eng-202402-p40041/output/")
+dfCli = spark.sql("select * from ecommerce.clientes_parquet")
+dfCli.show(5)
+
+dfPed = spark.sql("select * from ecommerce.pedidos_parquet")
+dfPed.show(5)
+
+dfPed.write.format("json").mode("overwrite").save("s3://lab-data-eng-202402-p40041/output/")
