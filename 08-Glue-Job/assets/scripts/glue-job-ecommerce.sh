c5e4bfe5f9d90039ca7650d8f219a00bd947a30e
infobarbosa <infobarbosa@yahoo.com.br>
2024-01-25 16:25:13 -0300

    Update S3 script path and default arguments in Glue job. Escapando as aspas simples pra interpolar a variável de ambiente BUCKET_NAME

--- 08-Glue-Job/assets/scripts/glue-job-ecommerce.sh
+++ 08-Glue-Job/assets/scripts/glue-job-ecommerce.sh
@@ -4,7 +4,7 @@ export BUCKET_NAME=$(aws s3api list-buckets --query "Buckets[].Name" | grep 'lab
 echo $BUCKET_NAME
 
 # Copiando o script para o S3
-aws s3 cp 08-Glue-Job/assets/scripts/glue-job-ecommerce.py s3://lab-data-eng-202402-p40041/scripts/glue-job-ecommerce.py
+aws s3 cp 08-Glue-Job/assets/scripts/glue-job-ecommerce.py s3://${BUCKET_NAME}/scripts/glue-job-ecommerce.py
 
 # Criando as pastas no S3
 aws s3api put-object --bucket ${BUCKET_NAME} --key temp/
@@ -15,6 +15,7 @@ aws glue create-job \
     --name ecommerce_glue_job \
     --role LabRole \
     --command Name=glueetl,ScriptLocation=s3://${BUCKET_NAME}/scripts/glue-job-ecommerce.py \
-    --default-arguments '{"--TempDir": "s3://\'${BUCKET_NAME}\'/temp", "--spark-event-logs-path": "s3://\'${BUCKET_NAME}\'/spark-ui/"}' \
+    --default-arguments '{"--TempDir": "s3://'"${BUCKET_NAME}"'/temp", "--spark-event-logs-path": "s3://'"${BUCKET_NAME}"'/spark-ui/"}' \
     --cli-input-json "file://08-Glue-Job/assets/scripts/glue-job-ecommerce.json"
+
 
