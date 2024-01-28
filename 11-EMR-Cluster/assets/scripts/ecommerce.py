37a8ad236b241a962403d96ebfc4f5b96f45db5c
infobarbosa <infobarbosa@yahoo.com.br>
2024-01-28 15:16:00 -0300

    Add EMR cluster script for data processing

--- 11-EMR-Cluster/assets/scripts/ecommerce.py
+++ 11-EMR-Cluster/assets/scripts/ecommerce.py
@@ -6,14 +6,17 @@ from datetime import datetime
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
+print("Iniciando o script de processamento dos dados: data-eng-lab")
 spark = SparkSession \
     .builder \
     .appName("data-eng-lab") \
+    .config("hive.metastore.client.factory.class", "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory") \
+    .enableHiveSupport() \
     .getOrCreate()
 
 spark.catalog.setCurrentDatabase("ecommerce")
 
-print("Definindo a variável s3_bucket que vamos utilizar ao longo do código")
+print("Definindo a variavel s3_bucket que vamos utilizar ao longo do codigo")
 BUCKET_NAME = ""
 s3_client = boto3.client('s3')
 response = s3_client.list_buckets()
@@ -22,16 +25,16 @@ for bucket in response['Buckets']:
     if bucket['Name'].startswith('lab-data-eng-'):
         BUCKET_NAME = bucket['Name']
 
-print("O bucket que vamos utilizar é: " + BUCKET_NAME)
+print("O bucket que vamos utilizar serah: " + BUCKET_NAME)
 
-dfCli = spark.sql("select * from ecommerce.clientes_parquet")
-dfCli.show(5)
-
+print("Obtendo os dados de pedidos")
 dfPed = spark.sql("select * from ecommerce.pedidos_parquet")
 dfPed.show(5)
 
+print("Escrevendo os dados de pedidos no S3")
 dfPed.write.format("json").mode("overwrite").save("s3://" + BUCKET_NAME + "/output/pedidos")
 
+print("Executando consulta dos top 10 clientes")
 dfTop10 = spark.sql(
  """SELECT cli.nome, cli.email, sum(ped.quantidade * ped.valor_unitario) total
     FROM clientes_parquet cli
@@ -43,4 +46,7 @@ dfTop10 = spark.sql(
 
 dfTop10.show(10)
 
+print("Escrevendo os dados dos top 10 clientes no S3")
 dfTop10.write.format("json").mode("overwrite").save("s3://" + BUCKET_NAME + "/output/top10/")
+
+print("job finalizado")
