ffdcb1ac7858677d35b07f3074ad1396e239925d
infobarbosa <infobarbosa@yahoo.com.br>
2024-02-13 16:31:32 -0300

    Add Glue Catalog setup script with S3 locations

--- 04-Glue-Catalog/assets/scripts/setup.sh
+++ 04-Glue-Catalog/assets/scripts/setup.sh
@@ -5,6 +5,9 @@
 
 # 04 - Glue Catalog
 
+export BUCKET_NAME=$(aws s3api list-buckets --query "Buckets[].Name" | grep 'lab-data-eng' | tr -d ' ' | tr -d '"' | tr -d ',')
+export CLIENTES_S3_LOCATION="s3://${BUCKET_NAME}/raw/ecommerce/clientes/"
+export PEDIDOS_S3_LOCATION="s3://${BUCKET_NAME}/raw/ecommerce/pedidos/"
 
 echo "Database ecommerce"
 aws glue create-database --database-input "{\"Name\":\"ecommerce\"}" 
@@ -12,11 +15,16 @@ aws glue create-database --database-input "{\"Name\":\"ecommerce\"}"
 echo "Database criado"
 aws glue get-databases
 
-
 echo "Tabela tb_raw_clientes"
+jq --arg S3_LOCATION "$CLIENTES_S3_LOCATION" '.StorageDescriptor.Location = $S3_LOCATION' 04-Glue-Catalog/assets/scripts/clientes.json > /tmp/clientes.json.tmp
+mv /tmp/clientes.json.tmp 04-Glue-Catalog/assets/scripts/clientes.json
+
 aws glue create-table --database-name ecommerce --table-input "file://04-Glue-Catalog/assets/scripts/clientes.json"
 
 echo "Tabela tb_raw_pedidos"
+jq --arg S3_LOCATION "$PEDIDOS_S3_LOCATION" '.StorageDescriptor.Location = $S3_LOCATION' 04-Glue-Catalog/assets/scripts/pedidos.json > /tmp/pedidos.json.tmp
+mv /tmp/pedidos.json.tmp 04-Glue-Catalog/assets/scripts/pedidos.json
+
 aws glue create-table --database-name ecommerce --table-input "file://04-Glue-Catalog/assets/scripts/pedidos.json"
 
 echo "Tabelas criadas"
