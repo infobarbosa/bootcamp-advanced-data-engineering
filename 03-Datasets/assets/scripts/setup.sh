6b9b1d9fc183a1f0b1a86bf7ed08a48baed5839d
infobarbosa <infobarbosa@yahoo.com.br>
2024-02-13 15:40:01 -0300

    Update setup.sh script for uploading datasets

--- 03-Datasets/assets/scripts/setup.sh
+++ 03-Datasets/assets/scripts/setup.sh
@@ -13,14 +13,14 @@ export BUCKET_NAME=$(aws s3api list-buckets --query "Buckets[].Name" | grep 'lab
 
 echo "Nome do bucket: $BUCKET_NAME"
 
-# Executando o upload da base de clientes
+echo "Executando o upload da base de clientes:"
 aws s3 cp 03-Datasets/assets/data/clientes.csv.gz s3://${BUCKET_NAME}/raw/ecommerce/clientes/ 
 
-# Conferindo se o upload ocorreu como esperado:
+echo "Conferindo se o upload ocorreu como esperado:"
 aws s3 ls s3://${BUCKET_NAME}/raw/ecommerce/clientes/
 
-# Executando o upload da base de pedidos
+echo "Executando o upload da base de pedidos:"
 aws s3 cp 03-Datasets/assets/data/ s3://${BUCKET_NAME}/raw/ecommerce/pedidos/ --recursive --exclude "*" --include "pedidos*"
 
-# Conferindo se o upload ocorreu como esperado:
+echo "Conferindo se o upload ocorreu como esperado:"
 aws s3 ls s3://${BUCKET_NAME}/raw/ecommerce/pedidos/
