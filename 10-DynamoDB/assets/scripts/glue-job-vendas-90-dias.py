f4dd6ad7b4458ea0b943c50adb9780984ee6002a
infobarbosa <infobarbosa@yahoo.com.br>
2024-02-13 20:21:20 -0300

    Add Glue job and setup script for calculating sales in the last 90 days

---
+++ 10-DynamoDB/assets/scripts/glue-job-vendas-90-dias.py
@@ -0,0 +1,76 @@
+
+import sys
+import boto3
+import time
+from awsglue.transforms import *
+from pyspark.sql.functions import sum, col, desc, count
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame  
+
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("boto3").setLevel(logging.WARNING)
+logging.getLogger("botocore").setLevel(logging.WARNING)
+
+logging.info("Criando o contexto do Glue")
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+logging.info("Contexto do Glue criado com sucesso")
+
+logging.info("Criando o dataframe de clientes")
+dyfClientes = glueContext.create_dynamic_frame.from_catalog(database='ecommerce', table_name='clientes_parquet')
+dyfClientes.printSchema()
+
+logging.info("Renomeando o atributo ID para evitar colisão com o outro dataset e eliminando os atributos CPF e data_nasc")
+dyfClientes = dyfClientes.drop_fields(["cpf","data_nasc"])
+dyfClientes = dyfClientes.rename_field("id", "id_cli")
+dyfClientes.toDF().show(5)
+
+logging.info("Criando o dataframe de pedidos considerando apenas os últimos 90 dias")
+dyfPedidos = glueContext.create_dynamic_frame.from_catalog(database='ecommerce', table_name='pedidos_parquet', push_down_predicate = "CAST(data_pedido AS DATE) >= CURRENT_DATE - INTERVAL '90' DAY")
+dyfPedidos.printSchema()
+dyfPedidos.toDF().show(5)
+
+logging.info("Eliminando atributo produto, desnecessário nesta operação")
+dyfPedidos = dyfPedidos.drop_fields(["produto"])
+dyfPedidos.toDF().show(5)
+
+logging.info("Criando um novo atributo VALOR_TOTAL")
+dfPedidos = dyfPedidos.toDF()
+dfPedidos = dfPedidos.withColumn("valor_total", col("quantidade") * col("valor_unitario"))
+dyfPedidos = DynamicFrame.fromDF(dfPedidos, glueContext, "dyfPedidos")
+
+logging.info("Eliminando os atributos quantidade e valor_unitario")
+dyfPedidos = dyfPedidos.drop_fields(["quantidade", "valor_unitario"])
+
+logging.info("Join de dyfPedidos com dyfClientes para obter o nome e email dos clientes")
+dyfClientesPedidos = dyfClientes.join(paths1=["id_cli"], paths2=["id_cliente"], frame2=dyfPedidos)
+dyfClientesPedidos.toDF().show(5)
+
+logging.info("Converte para dataframe para realizar a agregação")
+dfClientesPedidos = dyfClientesPedidos.toDF()
+
+dfVendas90Dias = dfClientesPedidos.groupBy("id_cliente", "nome", "email") \
+        .agg(sum("valor_total").alias("valor_total")) \
+        .agg(count("id_pedido").alias("qtd_pedidos")) 
+
+dyfVendas90Dias = DynamicFrame.fromDF(dfVendas90Dias, glueContext, "dyfVendas90Dias")
+
+logging.info("Escrevendo o dataframe no DynamoDB")
+glueContext.write_dynamic_frame.from_options(
+    frame = DynamicFrame.fromDF(dyfVendas90Dias, glueContext, "nested"),
+    connection_type = "dynamodb",
+    connection_options = {"dynamodb.output.tableName": "vendas_90_dias",
+                            "dynamodb.throughput.write.percent": "1.0",
+                            "dynamodb.region": "us-east-1",
+                            "dynamodb.output.retry": "100",
+                            "dynamodb.output.numparalleltasks": "1"}
+)
+
+job.commit()
---
+++ 10-DynamoDB/assets/scripts/setup.sh
@@ -0,0 +1,30 @@
+# Bootcamp Advanced Data Engineering
+# Author: Prof. Barbosa<br>
+# Contact: infobarbosa@gmail.com<br>
+# Github: [infobarbosa](https://github.com/infobarbosa)
+
+# Variável de ambiente BUCKET_NAME
+export BUCKET_NAME=$(aws s3api list-buckets --query "Buckets[].Name" | grep 'lab-data-eng' | tr -d ' ' | tr -d '"' | tr -d ',')
+
+echo "Bucket: $BUCKET_NAME"
+
+echo "Copiando os scripts para o S3"
+aws s3 cp 10-DynamoDB/assets/scripts/glue-job-vendas-90-dias.py s3://${BUCKET_NAME}/scripts/glue-job-vendas-90-dias.py
+
+echo "Criando as pastas no S3"
+aws s3api put-object --bucket ${BUCKET_NAME} --key temp/
+aws s3api put-object --bucket ${BUCKET_NAME} --key spark-ui/
+
+echo "Criando o job"
+aws glue create-job \
+    --name glue-job-vendas-90-dias \
+    --role LabRole \
+    --command Name=glueetl,ScriptLocation=s3://${BUCKET_NAME}/scripts/glue-job-vendas-90-dias.py \
+    --default-arguments '{"--TempDir": "s3://'"${BUCKET_NAME}"'/temp","--enable-spark-ui": "true","--spark-event-logs-path": "s3://'"${BUCKET_NAME}"'/spark-ui/","--enable-metrics":"true","--enable-job-insights":"true","--enable-continuous-cloudwatch-log":"true","--job-language":"python"}' \
+    --cli-input-json "file://08-Glue-Job/assets/scripts/glue-job-vendas-90-dias.json"
+
+
+echo "Executando o job"
+aws glue start-job-run --job-name glue-job-vendas-90-dias
+
+echo "Glue jobs iniciados com sucesso!"
