2553d7e15e5361ada94f0b6c408a748f3a5f677e
infobarbosa <infobarbosa@yahoo.com.br>
2024-02-18 10:52:21 -0300

    Update Glue job command to gluestreaming in setup.sh

--- 30-Kinesis/assets/scripts/setup.sh
+++ 30-Kinesis/assets/scripts/setup.sh
@@ -22,7 +22,7 @@ echo "Criando o job"
 aws glue create-job \
     --name glue-job-pedidos-stream \
     --role LabRole \
-    --command Name=glueetl,ScriptLocation=s3://${BUCKET_NAME}/scripts/glue-job-pedidos-stream.py \
+    --command Name=gluestreaming,ScriptLocation=s3://${BUCKET_NAME}/scripts/glue-job-pedidos-stream.py \
     --default-arguments '{"--BUCKET_NAME"='"${BUCKET_NAME}"',"--STREAM_ARN"='"${STREAM_ARN}"',"--TEMP_DIR": "s3://'"${BUCKET_NAME}"'/temp","--enable-spark-ui": "true","--spark-event-logs-path": "s3://'"${BUCKET_NAME}"'/spark-ui/","--enable-metrics":"true","--enable-job-insights":"true","--enable-continuous-cloudwatch-log":"true","--job-language":"python"}' \
     --cli-input-json "file://30-Kinesis/assets/scripts/glue-job-pedidos-stream.json"
 
